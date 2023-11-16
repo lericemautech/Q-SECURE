2dd647937669ac5c7712ef09892c39a70b5d3ba1
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-11-16 18:57:55 -0500

    Minor changes

--- client.py
+++ client.py
@@ -1,27 +1,35 @@
-# matrix_client.py
-import socket
-import pickle
-import numpy as np
+from socket import socket, AF_INET, SOCK_STREAM
+from pickle import loads, dumps
+from numpy import ndarray, random
 
-def send_matrices(server_address, matrix_a, matrix_b):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# get small matrix client
+# call func to partition in main
+
+HOST = "127.0.0.1"
+PORT = 12345
+TIMEOUT = 5
+BUFFER = 4096
+LENGTH = 2
+
+def send_matrices(server_address: tuple, matrix_a: ndarray, matrix_b: ndarray) -> ndarray:
+    client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect(server_address)
 
     matrices = {'matrix_a': matrix_a, 'matrix_b': matrix_b}
-    client_socket.send(pickle.dumps(matrices))
+    client_socket.send(dumps(matrices))
 
-    data = client_socket.recv(4096)
-    result_matrix = pickle.loads(data)
+    data = client_socket.recv(BUFFER)
+    result_matrix = loads(data)
 
     client_socket.close()
     return result_matrix
 
 if __name__ == "__main__":
     # Example matrices for testing
-    matrix_a = np.random.rand(2, 2)
-    matrix_b = np.random.rand(2, 2)
+    matrix_a = random.rand(LENGTH, LENGTH)
+    matrix_b = random.rand(LENGTH, LENGTH)
 
-    server_address = ('127.0.0.1', 12345)
+    server_address = (HOST, PORT)
     result = send_matrices(server_address, matrix_a, matrix_b)
 
     print("Matrix A:")
--- server.py
+++ server.py
@@ -1,26 +1,31 @@
 # matrix_server.py
-import socket
-import pickle
-import numpy as np
+from socket import socket, AF_INET, SOCK_STREAM
+from pickle import loads, dumps
+from numpy import dot, ndarray
 
-def multiply_matrices(matrix_a, matrix_b):
-    return np.dot(matrix_a, matrix_b)
+HOST = "127.0.0.1"
+PORT = 12345
+TIMEOUT = 5
+BUFFER = 4096
 
-def handle_client(client_socket):
-    data = client_socket.recv(4096)
-    matrices = pickle.loads(data)
+def multiply_matrices(matrix_a: ndarray, matrix_b: ndarray) -> ndarray:
+    return dot(matrix_a, matrix_b)
+
+def handle_client(client_socket: socket) -> None:
+    data = client_socket.recv(BUFFER)
+    matrices = loads(data)
     print('Receive matrix a:', matrices['matrix_a'])
     print('Receive matrix b:', matrices['matrix_b'])
     result_matrix = multiply_matrices(matrices['matrix_a'], matrices['matrix_b'])
     print('This is the result:', result_matrix)
-    client_socket.send(pickle.dumps(result_matrix))
+    client_socket.send(dumps(result_matrix))
     client_socket.close()
 
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('127.0.0.1', 12345))
-    server_socket.listen(5)
-    print("Server listening on port 12345...")
+def start_server() -> None:
+    server_socket = socket(AF_INET, SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    server_socket.listen(TIMEOUT)
+    print(f"Server listening on Port {PORT}...")
 
     while True:
         client_socket, addr = server_socket.accept()

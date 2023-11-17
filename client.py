f953f95d0053825cd7d16a62b0297b8d230a4123
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-11-16 19:40:18 -0500

    Partitions dict update: Works for matrix * vector

--- client.py
+++ client.py
@@ -1,40 +1,106 @@
 from socket import socket, AF_INET, SOCK_STREAM
 from pickle import loads, dumps
-from numpy import ndarray, random
+from numpy import ndarray, random, array_split, array_equal, concatenate
 
 # get small matrix client
 # call func to partition in main
 
 HOST = "127.0.0.1"
 PORT = 12345
-TIMEOUT = 5
 BUFFER = 4096
-LENGTH = 2
+LENGTH = 8
+HORIZONTAL_PARTITIONS = 2
+VERTICAL_PARTITIONS = 2
+MIN = 0
+MAX = 5
 
-def send_matrices(server_address: tuple, matrix_a: ndarray, matrix_b: ndarray) -> ndarray:
+def partition(matrix: ndarray) -> list:
+    """
+    Partition matrix into submatrices
+
+    Args:
+        matrix (ndarray): Matrix to be partitioned
+
+    Returns:
+        list: Partitioned matrix
+    """
+    # Check if matrix is 1D (i.e. vector)
+    if matrix.ndim == 1: return array_split(matrix, VERTICAL_PARTITIONS, axis = 0)
+
+    # Split matrix horizontally
+    sub_matrices = array_split(matrix, HORIZONTAL_PARTITIONS, axis = 0)
+    
+    # Split submatrices vertically, then return
+    return [m for sub_matrix in sub_matrices for m in  array_split(sub_matrix, VERTICAL_PARTITIONS, axis = 1)]
+
+def combine_results(matrices: dict) -> ndarray:
+    """
+    Combines all separate submatrices into a single matrix
+
+    Args:
+        matrices (dict): Results of each partitioned matrices' multiple
+
+    Returns:
+        ndarray: Combined result of given matrices
+    """
+    combined_results, end = [], 0
+
+    # Get all results from the queue, sorted by its position
+    results = [value for _, value in sorted(matrices.items())]
+
+    # Sum all values in the same row, then add to combined_results
+    for i in range(0, len(results), VERTICAL_PARTITIONS):
+        end += VERTICAL_PARTITIONS
+        combined_results.append(sum(results[i:end]))
+
+    # Combine all results into a single matrix
+    return concatenate(combined_results)
+
+def send_matrices(server_address: tuple[str, int], matrix_a_partitions: list, matrix_b_partitions: list) -> dict:
+    """
+    Sends partitioned matrices to server
+
+    Args:
+        server_address (tuple[str, int]): Server's IP address and port number
+        matrix_a_partitions (list): Matrix A's partitions
+        matrix_b_partitions (list): Matrix B's partitions
+
+    Returns:
+        dict: Results of each partitioned matrices' multiple
+    """
+    results = [ ]
     client_socket = socket(AF_INET, SOCK_STREAM)
     client_socket.connect(server_address)
 
-    matrices = {'matrix_a': matrix_a, 'matrix_b': matrix_b}
+    matrices = {'matrix_a': matrix_a_partitions, 'matrix_b': matrix_b_partitions}
     client_socket.send(dumps(matrices))
-
     data = client_socket.recv(BUFFER)
-    result_matrix = loads(data)
+    results = loads(data)
 
     client_socket.close()
-    return result_matrix
+    return results
 
 if __name__ == "__main__":
     # Example matrices for testing
-    matrix_a = random.rand(LENGTH, LENGTH)
-    matrix_b = random.rand(LENGTH, LENGTH)
+    matrix_a = random.randint(MIN, MAX, size = (LENGTH, LENGTH))
+    matrix_b = random.randint(MIN, MAX, size = LENGTH)
+
+    matrix_a_partitions = partition(matrix_a)
+    matrix_b_partitions = partition(matrix_b)
 
     server_address = (HOST, PORT)
-    result = send_matrices(server_address, matrix_a, matrix_b)
+    result = send_matrices(server_address, matrix_a_partitions, matrix_b_partitions)
+    answer = combine_results(result)
 
     print("Matrix A:")
     print(matrix_a)
     print("\nMatrix B:")
     print(matrix_b)
     print("\nResult Matrix:")
-    print(result)
+    print(answer)
+
+    print("\nCorrect answer:", matrix_a @ matrix_b)
+
+    if array_equal(matrix_a @ matrix_b, answer):
+        print("\nSuccess!")
+    else: print("\nFailure!")
--- server.py
+++ server.py
@@ -1,4 +1,3 @@
-# matrix_server.py
 from socket import socket, AF_INET, SOCK_STREAM
 from pickle import loads, dumps
 from numpy import dot, ndarray
@@ -7,21 +6,42 @@ HOST = "127.0.0.1"
 PORT = 12345
 TIMEOUT = 5
 BUFFER = 4096
+VERTICAL_PARTITIONS = 2
 
 def multiply_matrices(matrix_a: ndarray, matrix_b: ndarray) -> ndarray:
+    """
+    Multiply 2 matrices
+
+    Args:
+        matrix_a (ndarray): Matrix A
+        matrix_b (ndarray): Matrix B
+
+    Returns:
+        ndarray: Multiple of Matrix A and Matrix B
+    """
     return dot(matrix_a, matrix_b)
 
 def handle_client(client_socket: socket) -> None:
+    """
+    Get partitions of Matrix A and Matrix B from client, multiply them, then send result back to client
+
+    Args:
+        client_socket (socket): Client socket
+    """
+    results = { }
     data = client_socket.recv(BUFFER)
     matrices = loads(data)
-    print('Receive matrix a:', matrices['matrix_a'])
-    print('Receive matrix b:', matrices['matrix_b'])
-    result_matrix = multiply_matrices(matrices['matrix_a'], matrices['matrix_b'])
-    print('This is the result:', result_matrix)
-    client_socket.send(dumps(result_matrix))
+    matrix_a_partitions, matrix_b_partitions = matrices["matrix_a"], matrices["matrix_b"]
+    for i in range(len(matrix_a_partitions)):
+        results[i] = multiply_matrices(matrix_a_partitions[i], matrix_b_partitions[i % VERTICAL_PARTITIONS])
+    print(f"These are the results: {results}\n")
+    client_socket.send(dumps(results))
     client_socket.close()
 
 def start_server() -> None:
+    """
+    Start server and listen for connections
+    """
     server_socket = socket(AF_INET, SOCK_STREAM)
     server_socket.bind((HOST, PORT))
     server_socket.listen(TIMEOUT)
@@ -29,7 +49,7 @@ def start_server() -> None:
 
     while True:
         client_socket, addr = server_socket.accept()
-        print(f"Accepted connection from {addr}")
+        print(f"Accepted connection from {addr}\n")
         handle_client(client_socket)
 
 if __name__ == "__main__":

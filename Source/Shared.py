bcbeb29aa12300b9ff86bab680d4a29b680652fe
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-12-11 15:59:16 -0500

    Remove unnecessary code in `Shared.py`

--- Source/Shared.py
+++ Source/Shared.py
@@ -1,38 +1,21 @@
 from socket import socket
 from typing import NamedTuple
-from numpy import ndarray, random, array_equal, concatenate
+from numpy import ndarray, random, array_equal
 
-TIMEOUT = 10
-BUFFER = 4096
-HEADERSIZE = 10
 MIN = 0
 MAX = 5
 LENGTH = 16
 MATRIX_2_WIDTH = 2
 HORIZONTAL_PARTITIONS = 2
 VERTICAL_PARTITIONS = 2
-
-class Matrix(NamedTuple):
-    matrix: ndarray
-    index: int
+BUFFER = 4096
+HEADERSIZE = 10
 
 class Address(NamedTuple):
     ip: str
     port: int
 
-def add_header(data: bytes) -> bytes:
-    """
-    Add header to data
-
-    Args:
-        data (bytes): Data to be sent
-
-    Returns:
-        bytes: Data with header
-    """
-    return bytes(f"{len(data):<{HEADERSIZE}}", "utf-8") + data
-
-def send_data(sock: socket, data: bytes) -> None:
+def send(sock: socket, data: bytes) -> None:
     """
     Send data to socket
 
@@ -41,12 +24,12 @@ def send_data(sock: socket, data: bytes) -> None:
         data (bytes): Data to be sent
     """
     # Add header to data packet
-    data_with_header = add_header(data)
+    data_with_header = bytes(f"{len(data):<{HEADERSIZE}}", "utf-8") + data
 
     # Send message back
     sock.sendall(data_with_header)
 
-def receive_data(sock: socket) -> bytes:
+def receive(sock: socket) -> bytes:
     """
     Receive data from socket
 
@@ -86,57 +69,4 @@ def generate_matrix(length: int, width: int) -> ndarray:
     Returns:
         ndarray: Random matrix of size length * width
     """
-    return random.randint(MIN, MAX, size = (length, width))
-
-def verify(result: ndarray, check: ndarray) -> bool:
-    """
-    Checks if result is correct
-
-    Args:
-        result (ndarray): Calculated result
-        check (ndarray): Numpy's result
-
-    Returns:
-        bool: True if correct, False otherwise
-    """
-    return array_equal(result, check)
-
-def print_outcome(result: ndarray, check: ndarray) -> None:
-    """
-    Prints calculation's outcome (i.e. correctness)
-
-    Args:
-        result (ndarray): Calculated result
-        check (ndarray): Numpy's result
-    """
-    if verify(result, check):
-        print("\nCORRECT CALCULATION!")
-        exit(1)
-
-    else:
-        print("\nINCORRECT CALCULATION...")
-        exit(0)
-
-def combine_results(matrix_products: dict) -> ndarray:
-    """
-    Combines all separate submatrices into a single matrix
-
-    Args:
-        matrix_products (dict): Dictionary to store results (i.e. Value = Chunk of Matrix A * Chunk of Matrix B at Key = given position)
-
-    Returns:
-        ndarray: Combined result of given matrices
-    """
-    # Declare list for storing combined results, and end index
-    combined_results, end = [], 0
-
-    # Get all results from the queue, sorted by its position
-    results = [value for _, value in sorted(matrix_products.items())]
-
-    # Sum all values in the same row, then add to combined_results
-    for i in range(0, len(results), VERTICAL_PARTITIONS):
-        end += VERTICAL_PARTITIONS
-        combined_results.append(sum(results[i:end]))
-
-    # Combine all results into a single matrix
-    return concatenate(combined_results)
+    return random.randint(MIN, MAX, size = (length, width), dtype = int)

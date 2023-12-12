584334263726595efa31c4ee1b3602c7f741c229
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-12-12 17:49:11 -0500

    Create `RSA.py`

---
+++ Source/RSA.py
@@ -0,0 +1,14 @@
+PRIME_NUM_LENGTH = 64
+
+class RSA():
+    def __init__(self):
+        self._key: bytes
+
+    def encrypt(self, data: bytes):
+        print()
+
+    def decrypt(self):
+        print()
+
+if __name__ == "__main__":
+    cipher = RSA()

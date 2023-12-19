49c69e2e2315687db41152fca6db6e169fdbbbcd
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-12-19 17:05:05 -0500

    Remove whitespace in `TestServer1.py`

--- project/test/TestServer1.py
+++ project/test/TestServer1.py
@@ -2,7 +2,6 @@ from project.src.Server import Server as Server
 from project.src.Client import ADDRESSES
 
 if __name__ == "__main__":
-    
     # Create server at 1st port in PORTS list
     server = Server(ADDRESSES[0])
 

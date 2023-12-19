33b385db034ec45dc34714f653915d2670a1db0a
Kiran Brahmatewari <kiwi2mii@gmail.com>
2023-12-19 17:04:50 -0500

    Delete redundant `main.py`

--- project/src/main.py
+++
@@ -1,9 +0,0 @@
-from project.src.Server import Server
-from project.src.Client import ADDRESSES
-
-if __name__ == "__main__":
-    # Create server at 1st port in PORTS list
-    server = Server(ADDRESSES[0])
-
-    # Start server
-    server.start_server()

499bc033efd4f4b0827b04e59da6f91fae5da4dc
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-01-04 20:26:34 -0500

    Refactoring and support for logging

--- project/src/Shared.py
+++ project/src/Shared.py
@@ -1,17 +1,19 @@
 from socket import socket
 from typing import NamedTuple
-from numpy import ndarray, random
+from numpy import ndarray, random, array_split
 from os import getcwd, path
 
 MIN = 0
 MAX = 5
-LENGTH = 16
-HORIZONTAL_PARTITIONS = 2
-VERTICAL_PARTITIONS = 2
+LENGTH = 64
+HORIZONTAL_PARTITIONS = 32
+VERTICAL_PARTITIONS = 2 # Ensure VERTICAL_PARTITIONS is NOT greater than MATRIX_2_WIDTH
 BUFFER = 4096
 HEADERSIZE = 10
 DIRECTORY_PATH = path.join(getcwd(), "project", "file")
 FILENAME = "server_info.txt"
+LOG_CONF = "log.conf"
+LOG_CONF_PATH = path.join(DIRECTORY_PATH, "logging", LOG_CONF)
 
 class Address(NamedTuple):
     """
@@ -22,6 +24,23 @@ class Address(NamedTuple):
     """
     ip: str
     port: int
+
+def partition(matrix_a: ndarray, matrix_b: ndarray) -> tuple[list[ndarray], list[ndarray]]:
+    """
+    Partition Matrix A and Matrix B into submatrices
+
+    Args:
+        matrix_a (ndarray): Matrix A to be partitioned
+        matrix_b (ndarray): Matrix B to be partitioned
+
+    Returns:
+        tuple[list[ndarray], list[ndarray]]: Partitioned Matrix A and Matrix B
+    """
+    # Split matrix horizontally
+    sub_matrices = array_split(matrix_a, HORIZONTAL_PARTITIONS, axis = 0)
+    
+    # Split submatrices vertically, then return
+    return [m for sub_matrix in sub_matrices for m in  array_split(sub_matrix, VERTICAL_PARTITIONS, axis = 1)], array_split(matrix_b, VERTICAL_PARTITIONS, axis = 0)
 
 def send(sock: socket, data: bytes) -> None:
     """

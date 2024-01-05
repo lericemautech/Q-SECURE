3a15491492bd7f8631d4a12a3bdbae5f0f47244e
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-01-05 16:23:23 -0500

    Remove loggers from `TestServer`s

--- project/test/TestServer2.py
+++ project/test/TestServer2.py
@@ -1,11 +1,9 @@
-from project.src.Server import SERVER_LOGGER, Server as Server
+from project.src.Server import Server as Server
 from project.src.Client import ADDRESSES
 
 if __name__ == "__main__":
-    SERVER_LOGGER.info("Initializing Server 2...")
     # Create server at 2nd Address
     server = Server(ADDRESSES[1])
 
     # Start server
-    SERVER_LOGGER.info("Starting Server 2...")
     server.start_server()
--- project/test/TestServer3.py
+++ project/test/TestServer3.py
@@ -1,11 +1,9 @@
-from project.src.Server import SERVER_LOGGER, Server as Server
+from project.src.Server import Server as Server
 from project.src.Client import ADDRESSES
 
 if __name__ == "__main__":
-    SERVER_LOGGER.info("Initializing Server 3...")
     # Create server at 3rd Address
     server = Server(ADDRESSES[2])
 
     # Start server
-    SERVER_LOGGER.info("Starting Server 3...")
     server.start_server()

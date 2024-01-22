8afb15b3688642f87b79a6ea778e0f95996eb83f
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-01-22 17:06:52 -0500

    Update `Client.py`

--- project/src/Client.py
+++ project/src/Client.py
@@ -1,16 +1,15 @@
-from socket import socket, error, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
+from socket import socket, AF_INET, SOCK_STREAM, SOL_SOCKET, SO_REUSEADDR
 from pickle import loads, dumps
-from numpy import ndarray, random, array_equal, concatenate, dot, shape
+from numpy import ndarray, random, array_split, concatenate, dot, shape, array_equal
 from queue import Queue
 from os import path
 from random import sample
 from time import perf_counter
 from logging import getLogger, shutdown
-from logging.config import fileConfig
-from project.src.Shared import Address, receive, send, generate_matrix, partition, timing, FILE_DIRECTORY_PATH, FILENAME, HEADERSIZE, LENGTH, LOG_CONFIG_PATH, VERTICAL_PARTITIONS
+from project.src.ExceptionHandler import handle_exceptions
+from project.src.Shared import Address, HORIZONTAL_PARTITIONS, FILEPATH, create_logger, receive, send, generate_matrix, timing, HEADERSIZE, LENGTH, VERTICAL_PARTITIONS
 
 MATRIX_2_WIDTH = 2
-CLIENT_LOG = "client.log"
 CLIENT_LOGGER = getLogger(__name__)
 # TODO Relocate ADDRESSES to separate file for improved security and editing
 ADDRESSES = [ Address("127.0.0.1", 12345), Address("127.0.0.1", 12346), Address("127.0.0.1", 12347) ]
@@ -19,7 +18,7 @@ ADDRESSES = [ Address("127.0.0.1", 12345), Address("127.0.0.1", 12346), Address(
 class Client():
     def __init__(self, matrix_a: ndarray, matrix_b: ndarray, addresses: list[Address] = ADDRESSES):
         # Logging
-        fileConfig(LOG_CONFIG_PATH, defaults = { "logfilename" : CLIENT_LOG, "dirpath" : FILE_DIRECTORY_PATH }, disable_existing_loggers = False)
+        create_logger("client.log")
         CLIENT_LOGGER.info("Starting Client...\n")
 
         # Store server(s) results (i.e. Value = Chunk of Matrix A * Chunk of Matrix B at Key = given position)
@@ -28,18 +27,15 @@ class Client():
         # Select random number N between 1 and # of Servers, inclusive
         self._num_servers: int = random.randint(1, len(addresses) + 1)
 
-        # Select random subset of server(s) to send jobs to        
-        self._server_addresses: list[Address] = self._select_servers(addresses, self._num_servers)
-        
-        # Queue of partitions of Matrix A and Matrix B and their position, to be sent to selected server(s)
-        self._partitions: Queue = self._queue_partitions(matrix_a, matrix_b)
+        # Server(s) to send jobs to        
+        self._server_addresses: list[Address] = self._select_servers(addresses)
 
-        # Determine which selected server(s) to send jobs to; initialize all selected server(s) with 0 points
-        self._server_reliability: dict[Address, int] = { address: 0 for address in self._server_addresses }
+        # Create and queue partitions of Matrix A and Matrix B and their position, to be sent to selected server(s)
+        self._partitions: Queue = self._queue_partitions(matrix_a, matrix_b)
 
     def _queue_partitions(self, matrix_a: ndarray, matrix_b: ndarray) -> Queue:
         """
-        Add partitions of Matrix A and Matrix B and their position to queue
+        Create and queue partitions of Matrix A and Matrix B and their position
 
         Args:
             matrix_a (ndarray): Matrix A
@@ -48,15 +44,18 @@ class Client():
         Returns:
             Queue: Queue of partitions of Matrix A and Matrix B and their position
         """
-        # Get partitions of Matrix A and Matrix B
-        matrix_a_partitions, matrix_b_partitions = partition(matrix_a, matrix_b, CLIENT_LOGGER)
-
+        # Start timer
         start = perf_counter()
         
+        # Split matrix horizontally
+        sub_matrices = array_split(matrix_a, HORIZONTAL_PARTITIONS, axis = 0)
+
+        # Split submatrices vertically
+        matrix_a_partitions, matrix_b_partitions = [m for sub_matrix in sub_matrices for m in  array_split(sub_matrix, VERTICAL_PARTITIONS, axis = 1)], array_split(matrix_b, VERTICAL_PARTITIONS, axis = 0)
+
         # Declare queue to be populated and returned
         queue = Queue()
-
-        # Add partitions of Matrix A and Matrix B and their position to queue
+        
         for i in range(len(matrix_a_partitions)):
             # Have client compute some of the partitions; add 1 to num_servers to account for client
             if i % (self._num_servers + 1) == 0:
@@ -66,8 +65,9 @@ class Client():
             else:
                 queue.put((matrix_a_partitions[i], matrix_b_partitions[i % len(matrix_b_partitions)], i))
 
+        # End timer
         end = perf_counter()
-        CLIENT_LOGGER.info(f"Queued partitions in {timing(end, start)} seconds\n")
+        CLIENT_LOGGER.info(f"Created partitions and queue in {timing(end, start)} seconds\n")
         
         return queue
 
@@ -81,6 +81,7 @@ class Client():
         Returns:
             ndarray: Combined result of given matrices
         """
+        # Start timer
         start = perf_counter()
         
         # Get all results from the queue, sorted by its position
@@ -96,7 +97,8 @@ class Client():
 
         # Combine all results into a single matrix
         combined_results = concatenate(combined_results)
-
+        
+        # End timer
         end = perf_counter()
         CLIENT_LOGGER.info(f"Combined submatrices into a single matrix in {timing(end, start)} seconds\n")
         
@@ -111,14 +113,16 @@ class Client():
         """
         CLIENT_LOGGER.info(f"Generated number of servers to send jobs to = {self._num_servers}\n")
 
+        # Start timer
         start = perf_counter()
         
         # Send partitioned matrices to randomly selected server(s)
         self._work()
 
-        # Get [all] results combined into a single matrix
+        # Combine [all] results into a single matrix
         result = self._combine_results(self._matrix_products)
 
+        # End timer
         end = perf_counter()
         CLIENT_LOGGER.info(f"Calculated final result in {timing(end, start)} seconds\n")
 
@@ -144,7 +148,7 @@ class Client():
         send(server_socket, data)
 
         end_send = perf_counter()
-        CLIENT_LOGGER.info(f"Sent data in {timing(end_send, start_send)} seconds\n")
+        CLIENT_LOGGER.info(f"Data sent in {timing(end_send, start_send)} seconds\n")
         start_receive = perf_counter()
         
         # Receive acknowledgment from server
@@ -154,10 +158,10 @@ class Client():
         ack_msg_length = int(ack_data.decode("utf-8").strip())
         ack_msg = server_socket.recv(ack_msg_length).decode("utf-8").strip()
         if ack_msg != "ACK":
-            error_msg = f"Invalid acknowledgment \"{ack_msg}\""
-            CLIENT_LOGGER.error(error_msg)
+            exception_msg = f"Invalid acknowledgment \"{ack_msg}\""
+            CLIENT_LOGGER.exception(exception_msg)
             shutdown()
-            raise ValueError(error_msg)
+            raise ValueError(exception_msg)
                             
         # Receive data from server
         data = receive(server_socket)
@@ -167,13 +171,12 @@ class Client():
 
         return data
 
-    def _select_servers(self, addresses: list[Address], num_servers: int) -> list[Address]:
+    def _select_servers(self, addresses: list[Address]) -> list[Address]:
         """
         Selects a subset of server(s) with the highest compute power (i.e. most CPUs) to send jobs to
 
         Args:
             addresses (list[Address]): List of server addresses
-            num_servers (int): Amount of servers to send jobs to
 
         Raises:
             ValueError: Invalid number of servers
@@ -182,73 +185,69 @@ class Client():
             
         Returns:
             list[Address]: List of server addresses to send jobs to
-        """
-        # TODO
-        selected_servers = { }
-        
-        if num_servers > len(addresses) or num_servers < 1:
-            error_msg = f"{num_servers} is an invalid number of servers"
-            CLIENT_LOGGER.error(error_msg)
+        """        
+        if self._num_servers > len(addresses) or self._num_servers < 1:
+            exception_msg = f"{self._num_servers} is an invalid number of servers"
+            CLIENT_LOGGER.exception(exception_msg)
             shutdown()
-            raise ValueError(error_msg)
+            raise ValueError(exception_msg)
 
-        filepath = path.join(FILE_DIRECTORY_PATH, FILENAME)
-
-        if not path.exists(filepath):
-            error_msg = f"File {FILENAME} at {FILE_DIRECTORY_PATH} does not exist"
-            CLIENT_LOGGER.error(error_msg)
+        if not path.exists(FILEPATH):
+            exception_msg = f"File at {FILEPATH} does not exist"
+            CLIENT_LOGGER.exception(exception_msg)
             shutdown()
-            raise FileNotFoundError(error_msg)
+            raise FileNotFoundError(exception_msg)
 
-        if path.getsize(filepath) == 0:
-            error_msg = f"File {FILENAME} at {FILE_DIRECTORY_PATH} is empty"
-            CLIENT_LOGGER.error(error_msg)
+        if path.getsize(FILEPATH) == 0:
+            exception_msg = f"File at {FILEPATH} is empty"
+            CLIENT_LOGGER.exception(exception_msg)
             shutdown()
-            raise IOError(error_msg)
+            raise IOError(exception_msg)
 
-        # Read file containing server addresses and their CPU
-        with open(filepath, "r") as file:
+        server_cpu, server_ram = { }, { }
+
+        # Read file containing server addresses, their CPU, and available RAM
+        with open(FILEPATH, "r") as file:
             for line in file:
-                # Get IP Address, port, and CPU of server
-                curr_ip, curr_port, curr_cpu = line.split(" ")[:3]
+                # Get IP Address, port, CPU, and available RAM of server
+                curr_ip, curr_port, curr_cpu, curr_ram = line.split(" ")[:4]
                 curr_address = Address(curr_ip, int(curr_port))
 
-                # Add server address and CPU to dict if not already in it
-                if curr_address not in selected_servers.keys():
-                    selected_servers[curr_address] = int(curr_cpu)
+                # Add server address, its CPU, and available RAM to dict if not already in it
+                if curr_address not in (server_cpu.keys() or server_ram.keys()):
+                    server_cpu[curr_address] = curr_cpu
+                    server_ram[curr_address] = curr_ram
 
-        # Check if all selected servers have the same CPU power
-        if len(set(selected_servers.values())) == 1:
-            # If so, choose randomly from selected servers
-            return sample(list(selected_servers.keys()), num_servers)
+        # Check if all selected servers have the same CPU
+        if len(set(server_cpu.values())) == 1:
+            # Check if all selected servers with same CPU have the same available RAM
+            if len(set(server_ram.values())) == 1:
+                return sample(list(server_ram.keys()), self._num_servers)
+
+            # Otherwise, return the top servers (i.e. servers with most available RAM)
+            return sorted(server_ram, reverse = True)[:self._num_servers]
 
         # Otherwise, return the top servers (i.e. servers with highest CPU power)
-        return sorted(selected_servers, reverse = True)[:num_servers]
+        return sorted(server_cpu, reverse = True)[:self._num_servers]                
 
+    @handle_exceptions(CLIENT_LOGGER)
     def _work(self) -> None:
         """
         Send partitioned matrices to server(s), get results,
         then add them to dictionary for combining laters
-
-        Raises:
-            BrokenPipeError: Unable to write to shutdown socket
-            ConnectionRefusedError: Connection refused
-            ConnectionAbortedError: Connection aborted
-            ConnectionResetError: Connection reset
-            ConnectionError: Connection lost
-            TimeoutError: Connection timed out
         """
         # Index used to determine where to connect (i.e. cycles through available servers; round robin)
         i = 0
 
+        # Start method's timer
         start_work = perf_counter()
 
         # While there's still partitions to send to server(s)
         while not self._partitions.empty():
             try:
-                with socket(AF_INET, SOCK_STREAM) as client_socket:
+                with socket(AF_INET, SOCK_STREAM) as sock:
                     # Allow reuse of address
-                    client_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+                    sock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 
                     # Address of server
                     server_address = self._server_addresses[i % len(self._server_addresses)]
@@ -257,7 +256,7 @@ class Client():
                     start = perf_counter()
 
                     # Connect to server
-                    client_socket.connect(server_address)
+                    sock.connect(server_address)
                     connection_timer = perf_counter()
                     CLIENT_LOGGER.info(f"Client connected to Server at {server_address} in {timing(connection_timer, start)} seconds\n")
 
@@ -265,7 +264,7 @@ class Client():
                     partitions = self._partitions.get(timeout = 0.1)
 
                     # Receive result from server
-                    data = self._handle_server(client_socket, dumps(partitions))
+                    data = self._handle_server(sock, dumps(partitions))
                     
                     # Unpack data (i.e. product of partitions and its position) from server
                     result, index = loads(data)
@@ -273,17 +272,15 @@ class Client():
                     # End timer
                     end = perf_counter()
 
-                    CLIENT_LOGGER.info(f"Took Client {timing(end, start)} seconds to connect, send, receive, and unpack data from Server at {server_address}\n")
+                    CLIENT_LOGGER.info(f"Client connected, sent, received, and unpacked data from Server at {server_address} in {timing(end, start)} seconds\n")
 
                     # Check if result and index was received (i.e. not None)
                     if result is not None:
                         #print(f"Result Matrix from Server at {server_address} = {result}\n")
+                        CLIENT_LOGGER.info(f"Successfully received valid result from Server at {server_address}\n")
 
                         # Add result to dict, to be combined into final result later
                         self._matrix_products[index] = result
-
-                        # Increase server's reliability
-                        self._server_reliability[server_address] += 1
 
                     else:
                         CLIENT_LOGGER.error(f"Failed to receive valid result from Server at {server_address}; retrying later...\n")
@@ -291,81 +288,44 @@ class Client():
                         # Put partitions back into queue (since it was previously removed via .get()), to try again later
                         self._partitions.put(partitions)
 
-                        # Decrease server's reliability
-                        self._server_reliability[server_address] -= 1
-
-                    CLIENT_LOGGER.info(f"Server at {server_address} has {self._server_reliability[server_address]} points\n")
-
                     # Increment index
                     i += 1
-
-            except BrokenPipeError as exception:
-                exception_msg = "Unable to write to shutdown socket"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise BrokenPipeError(exception_msg) from exception
-
-            except ConnectionRefusedError as exception:
-                exception_msg = "Connection refused"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise ConnectionRefusedError(exception_msg) from exception
-
-            except ConnectionAbortedError as exception:
-                exception_msg = "Connection aborted"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise ConnectionAbortedError(exception_msg) from exception
-
-            except ConnectionResetError as exception:
-                exception_msg = "Connection reset"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise ConnectionResetError(exception_msg) from exception
-
-            except ConnectionError as exception:
-                exception_msg = "Connection lost"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise ConnectionError(exception_msg) from exception
-
-            except TimeoutError as exception:
-                exception_msg = "Connection timed out"
-                CLIENT_LOGGER.exception(exception_msg)
-                raise TimeoutError(exception_msg) from exception
-
-            except error as exception:
-                CLIENT_LOGGER.exception(exception)
-                raise error from exception
-
+            
             finally:
+                # End method's timer
                 end_work = perf_counter()
+
+                # Log Client's work time
                 CLIENT_LOGGER.info(f"Client worked for {timing(end_work, start_work)} seconds\n")
+                                
+                # Stop logging
                 shutdown()
 
-        # end_work = perf_counter()
-        # CLIENT_LOGGER.info(f"Client worked for {timing(end_work, start_work)} seconds")
+    def print_outcome(self, result: ndarray, check: ndarray) -> None:
+        """
+        Prints calculation's outcome (i.e. correctness)
 
-def print_outcome(result: ndarray, check: ndarray) -> None:
-    """
-    Prints calculation's outcome (i.e. correctness)
+        Args:
+            result (ndarray): Calculated result
+            check (ndarray): Numpy's result
+        """
+        if array_equal(result, check):
+            print("CORRECT CALCULATION!")
+            shutdown()
+            exit(0)
 
-    Args:
-        result (ndarray): Calculated result
-        check (ndarray): Numpy's result
-    """
-    if array_equal(result, check):
-        print("CORRECT CALCULATION!")
-        shutdown()
-        exit(0)
-
-    else:
-        print("INCORRECT CALCULATION...")
-        shutdown()
-        exit(1)
+        else:
+            print("INCORRECT CALCULATION...")
+            shutdown()
+            exit(1)
 
 if __name__ == "__main__":
     # Ensure 2nd matrix width is not smaller than number of vertical partitions
     if MATRIX_2_WIDTH < VERTICAL_PARTITIONS:
-        error_msg = f"2nd matrix's width ({MATRIX_2_WIDTH}) cannot be smaller than number of vertical partitions ({VERTICAL_PARTITIONS})"
-        CLIENT_LOGGER.error(error_msg)
+        exception_msg = f"2nd matrix's width ({MATRIX_2_WIDTH}) cannot be smaller than number of vertical partitions ({VERTICAL_PARTITIONS})"
+        CLIENT_LOGGER.exception(exception_msg)
         shutdown()
-        raise ValueError(error_msg)
+        raise ValueError(exception_msg)
     
     # Generate example matrices for testing
     matrix_a = generate_matrix(LENGTH, LENGTH)
@@ -382,4 +342,4 @@ if __name__ == "__main__":
     print(f"Final Result Matrix = {answer}\n")
 
     # Print outcome (i.e. answer's correctness)
-    print_outcome(answer, matrix_a @ matrix_b)
+    client.print_outcome(answer, matrix_a @ matrix_b)

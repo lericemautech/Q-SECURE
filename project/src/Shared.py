19c85ab46c5c7e667ca2e215e5df0417d2dc3cbc
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-01-22 17:06:46 -0500

    Update `Shared.py`

--- project/src/Shared.py
+++ project/src/Shared.py
@@ -1,9 +1,8 @@
 from socket import socket
 from typing import NamedTuple
-from numpy import ndarray, random, array_split, shape, array_equal, concatenate
+from numpy import ndarray, random
 from os import getcwd, path
-from logging import Logger, shutdown
-from time import perf_counter
+from logging.config import fileConfig
 
 MIN = 0
 MAX = 5
@@ -15,7 +14,8 @@ HEADERSIZE = 10
 SIG_FIGS = 5
 FILE_DIRECTORY_PATH = path.join(getcwd(), "project", "file")
 FILENAME = "server_info.txt"
-LOG_CONFIG_PATH = path.join(FILE_DIRECTORY_PATH, "logging", "log.conf")
+FILEPATH = path.join(FILE_DIRECTORY_PATH, FILENAME)
+TEMP_FILEPATH = path.join(FILE_DIRECTORY_PATH, f"temp_{FILENAME}")
 
 class Address(NamedTuple):
     """
@@ -27,74 +27,8 @@ class Address(NamedTuple):
     ip: str
     port: int
 
-def partition_1(matrix_a: ndarray, matrix_b: ndarray, weights: list[float], log: Logger) -> tuple[list[ndarray], list[ndarray]]:
-    # Start timer
-    start = perf_counter()
-    
-    # Matrix A's width
-    width = shape(matrix_a)[1]
-
-    # Confirm Matrix A's width == Matrix B's length
-    if width != shape(matrix_b)[0]:
-        error_msg = "Matrix A and Matrix B are incompatible"
-        log.error(error_msg)
-        shutdown()
-        raise ValueError(error_msg)
-
-    # Each partition's (i.e. submatrix's) width (i.e. percentage * Matrix A's width)
-    partition_widths = [ 1 if int(w * width) == 0 else int(w * width) for w in weights ]
-    
-    submatrices_a, submatrices_b, start, end = [], [], 0, 0
-
-    # Split Matrix A and Matrix B into submatrices
-    for w in partition_widths:
-        end += start + w
-        submatrices_a.append(matrix_a[:, start:end])
-        submatrices_b.append(matrix_b[start:end, :])
-        start = end
-
-    # Confirm both submatrices are valid (i.e. combining submatrices_a == Matrix A && combining submatrices_b == Matrix B)
-    if not array_equal(concatenate(submatrices_a, axis = 1), matrix_a) or not array_equal(concatenate(submatrices_b, axis = 0), matrix_b):
-        # End timer
-        end = perf_counter()
-        error_msg = "Invalid submatrices... concatenated submatrices != original matrices"
-        log.error(f"{error_msg}\n")
-        log.info(f"Total runtime = {timing(end, start)} seconds")
-        shutdown()
-        raise ValueError(error_msg)
-
-    # End timer
-    end = perf_counter()
-    log.info(f"Partitioned matrices in {timing(end, start)} seconds\n")
-    
-    return submatrices_a, submatrices_b
-
-def partition(matrix_a: ndarray, matrix_b: ndarray, log: Logger) -> tuple[list[ndarray], list[ndarray]]:
-    """
-    Partition Matrix A and Matrix B into submatrices
-
-    Args:
-        matrix_a (ndarray): Matrix A to be partitioned
-        matrix_b (ndarray): Matrix B to be partitioned
-        logger (Logger): Logger to log messages
-
-    Returns:
-        tuple[list[ndarray], list[ndarray]]: Partitioned Matrix A and Matrix B
-    """
-    # Start timer
-    start = perf_counter()
-    
-    # Split matrix horizontally
-    sub_matrices = array_split(matrix_a, HORIZONTAL_PARTITIONS, axis = 0)
-
-    # Split submatrices vertically
-    partitions = [m for sub_matrix in sub_matrices for m in  array_split(sub_matrix, VERTICAL_PARTITIONS, axis = 1)], array_split(matrix_b, VERTICAL_PARTITIONS, axis = 0)
-
-    # End timer
-    end = perf_counter()
-    log.info(f"Partitioned matrices in {timing(end, start)} seconds\n")
-    
-    return partitions
+def create_logger(log_name: str) -> None:
+    fileConfig(path.join(FILE_DIRECTORY_PATH, "logging", "log.conf"), defaults = { "logfilename" : log_name, "dirpath" : FILE_DIRECTORY_PATH }, disable_existing_loggers = False)
 
 def send(sock: socket, data: bytes) -> None:
     """

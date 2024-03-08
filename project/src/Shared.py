b71c7c2ecc5eb6ae6fa8fa52b861b7a932e7ce68
Kiran Brahmatewari <kiwi2mii@gmail.com>
2024-03-08 14:01:16 -0500

    Remove TLS constants from `Shared.py`

--- project/src/Shared.py
+++ project/src/Shared.py
@@ -20,21 +20,12 @@ ACKNOWLEDGEMENT = "ACK"
 """Socket Parameters"""
 
 FILE_DIRECTORY_PATH = path.join(getcwd(), "project", "file")
-KEYCHAIN_PATH = "/Users/kiran/.ssh/Certificates/Q-SECURE"
 LOG_PATH = path.join(FILE_DIRECTORY_PATH, "logging")
 """Parent Directory Paths"""
 
 MAX_NUM_FILES = 10
 FILEPATH = path.join(FILE_DIRECTORY_PATH, "server_info", "server_info.txt")
 """Server Info Parameters"""
-
-CERTIFICATE_AUTHORITY = path.join(KEYCHAIN_PATH, "ca-cert.cer.pem")
-CLIENT_CERT = path.join(KEYCHAIN_PATH, "client.cer.pem")
-CLIENT_KEY = path.join(KEYCHAIN_PATH, "client.key.pem")
-SERVER_CERT = path.join(KEYCHAIN_PATH, "server.cer.pem")
-SERVER_KEY = path.join(KEYCHAIN_PATH, "server.key.pem")
-TLS_LOG = path.join(LOG_PATH, "tls.log")
-"""SSL/TLS Parameters"""
 
 class Address(NamedTuple):
     """
